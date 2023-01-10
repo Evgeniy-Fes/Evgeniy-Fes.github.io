504d2ac995507e7ab3432c2431e7fb5d3e2783f2
mikavanko <mikhailivanko@gmail.com>
2023-01-11 01:31:38 +0300

    fixes

--- Device/src/js/main.js
+++ Device/src/js/main.js
@@ -1,1 +1,31 @@
-console.log('hello')
+
+$(function(){
+  $('.delivery-nav').click(function(e){
+    e.preventDefault();
+
+    $('.delivery-right__tab-content.active').removeClass('active');
+    $('.delivery-nav.active').removeClass('active');
+    
+    $(this).addClass('active');
+
+    const id = $(this).attr('href')
+
+    $(id).addClass('active');
+  })
+
+  $('.login').click(function(event){
+    console.log(event)
+
+    const header = $('.header');
+    header.animate({
+        opacity: '0.25',
+      }, 
+      1000,
+      function () {
+        header.animate({
+          opacity: '1',
+        })
+      }
+    )
+  })
+})
--- Device/src/js/slick.js
+++ Device/src/js/slick.js
@@ -1,6 +1,29 @@
 $(() => {
-  $('.slick-carousel').slick({
+  const slick = $('.slick-carousel').slick({
     arrows: true,
     dots: true,
   });
+
+  $('.foto-wrapper-arrow__left').click(function () {
+    $('.slick-carousel').slick('slickPrev')
+  })
+  $('.foto-wrapper-arrow__right').click(function () {
+    $('.slick-carousel').slick('slickNext')
+  })
+
+  const slideCount = $('.slick-carousel').slick('getSlick').slideCount
+  
+  for(let i = 0; i < slideCount; i++){
+    const template = `<div class="slider-decription__dot ${i === 0 ? 'active' : ''}"></div>`;
+    $('.slider-decription__dots').append(template)
+  }
+
+  $('.slider-decription__dot').click(function() {
+    const idx = $(this).index()
+    $('.slick-carousel').slick('slickGoTo', idx)
+
+    $('.slider-decription__dot.active').removeClass('active')
+
+    $(`.slider-decription__dot:nth-child(${idx+1})`).addClass('active')
+  })
 })

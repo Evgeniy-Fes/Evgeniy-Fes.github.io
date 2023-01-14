df1b16e8305ba12572766771c5b185a03f5d1c38
mikavanko <mikhailivanko@gmail.com>
2023-01-15 01:36:24 +0300

    fixes

--- Device/src/js/slick.js
+++ Device/src/js/slick.js
@@ -1,34 +1,43 @@
 $(() => {
-  const slick = $('.slick-img').slick({
-    asNavFor: '.slick-menu'
+  const slickImgClass = 'slick-img'
+  const slickImg = $(`.${slickImgClass}`)
+
+  slickImg.slick({
+    asNavFor: '.slick-menu',
+    arrows: true,
+    prevArrow: '<div class="foto-wrapper-arrow foto-wrapper-arrow__left"> &#8249; </div>',
+    nextArrow: '<div class="foto-wrapper-arrow foto-wrapper-arrow__right"> &#8250; </div>',
   });
 
   $('.foto-wrapper-arrow__left').click(function () {
-    $('.slick-img').slick('slickPrev')
+    slickImg.slick('slickPrev')
   })
   $('.foto-wrapper-arrow__right').click(function () {
-    $('.slick-img').slick('slickNext')
+    slickImg.slick('slickNext')
   })
-})
-
-$(() => {
-  const slick = $('.slick-menu').slick({
-    asNavFor: '.slick-img'
+  
+  const slickMenu = $('.slick-menu')
+  
+  slickMenu.slick({
+    asNavFor: `.${slickImgClass}`,
+    dots: true,
+    arrows: false,
+    dotsClass: 'slick-menu-dots',
   });
 
-  const slideCount = $('.slick-menu').slick('getSlick').slideCount
+//   const slideCount = slickMenu.slick('getSlick').slideCount
   
-  for(let i = 0; i < slideCount; i++){
-    const template = `<div class="slider-decription__dot ${i === 0 ? 'active' : ''}"></div>`;
-    $('.slider-decription__dots').append(template)
-  }
+//   for(let i = 0; i < slideCount; i++){
+//     const template = `<div class="slider-decription__dot ${i === 0 ? 'active' : ''}"></div>`;
+//     $('.slider-decription__dots').append(template)
+//   }
 
-  $('.slider-decription__dot').click(function() {
-    const idx = $(this).index()
-    $('.slick-menu').slick('slickGoTo', idx)
+//   $('.slider-decription__dot').click(function() {
+//     const idx = $(this).index()
+//     slickMenu.slick('slickGoTo', idx)
 
-    $('.slider-decription__dot.active').removeClass('active')
+//     $('.slider-decription__dot.active').removeClass('active')
 
-    $(`.slider-decription__dot:nth-child(${idx+1})`).addClass('active')
-  })
+//     $(`.slider-decription__dot:nth-child(${idx+1})`).addClass('active')
+//   })
 })

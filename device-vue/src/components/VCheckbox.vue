3a2bfc54b373fc6487c579455c3a1a6a4f6bb05b
mikavanko <mikhailivanko@gmail.com>
2023-03-01 00:25:55 +0300

    update checkbox component

---
+++ device-vue/src/components/VCheckbox.vue
@@ -0,0 +1,103 @@
+<template>
+  <label class="checkbox-label">
+    <input :checked="checked" :value="value" type="checkbox" class="real-checkbox" @change="updateInput">
+    <span class="custom-checkbox" />
+    {{ label }}
+  </label>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: 'checked',
+    event: 'change',
+  },
+  props: {
+    value: {
+      type: Number,
+      default: null,
+    },
+    checked: {
+      type: Boolean,
+      default: false,
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+  },
+  // computed: {
+  //   value: {
+  //     get() {
+  //       return this.$props.modelValue
+  //     },
+  //     set(val) {
+  //       this.$emit('update:modelValue', val)
+  //     }
+  //   }
+  // },
+  mounted() {
+  },
+  destroyed() {
+    console.log('destroyed')
+  },
+  methods: {
+    updateInput(e) {
+      let isChecked = e.target.checked
+      this.$emit('change', isChecked)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.checkbox-label{
+  cursor: pointer;
+}
+.real-checkbox {
+    width: 0;
+    height: 0;
+    opacity: 0;
+    position: absolute;
+    z-index: -1;
+}
+
+.real-checkbox:focus + .custom-checkbox {
+    box-shadow: 0px 0px 0px 1px #000000, 0px 0px 10px #000000;
+}
+
+.custom-checkbox {
+    position: relative;
+    display: inline-block;
+    width: 20px;
+    height: 20px;
+    background: #ffffff;
+    border: 2px solid #302d2d;
+    border-radius: 4px;
+    vertical-align: sub;
+    margin-right: 5px;
+}
+
+.custom-checkbox::before {
+    content: '';
+
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+
+    background-image: url(@/assets/VectorCheckbox.svg);
+    background-size: contain;
+    background-repeat: no-repeat;
+
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%) scale(0);
+    margin-top: 1px;
+    transition: 0.2s ease-in;
+}
+
+.real-checkbox:checked + .custom-checkbox::before {
+    transform: translate(-50%, -50%) scale(1);
+}
+</style>
--- device-vue/src/components/VFormCheckbox.vue
+++ device-vue/src/components/VFormCheckbox.vue
@@ -1,98 +1,87 @@
 <template>
-    <div>
-        <div>
-            <label>
-                <input checked type="checkbox" name="black" value="black" class="real-checkbox" v-model="color">
-                <span class="custom-checkbox"></span>
-                Чёрный
-            </label>
-        </div>
-        <div>
-            <label>
-                <input checked type="checkbox" name="white" value="white" class="real-checkbox" v-model="color">
-                <span class="custom-checkbox"></span>
-                Белый
-            </label>
-        </div>
-        <div>
-            <label>
-                <input type="checkbox" name="blue" value="blue" class="real-checkbox" v-model="color">
-                <span class="custom-checkbox"></span>
-                Синий
-            </label>
-        </div>
-        <div>
-            <label>
-                <input type="checkbox" name="red" value="red" class="real-checkbox" v-model="color">
-                <span class="custom-checkbox"></span>
-                Красный
-            </label>
-        </div>
-        <div>
-            <label>
-                <input type="checkbox" name="pink" value="pink" class="real-checkbox" v-model="color">
-                <span class="custom-checkbox"></span>
-                Розовый
-            </label>
-        </div>
+    <div class="checkbox-wrapper">
+        <VCheckbox 
+            v-for="item in checkboxes" 
+            :key="item.id" 
+            :label="item.label" 
+            :value="item.id"
+            v-model="item.checked"
+        />
+
+        <input v-model="firstName" />
+        <input v-model="lastName" />
+        <input v-model="middleName" />
+        <input :value="inputText" @input="updateInput" />
+        {{ fio }}
+        {{ inputText }}
     </div>
 </template>
 
 <script>
+import VCheckbox from './VCheckbox.vue';
+
+let a = '123';
+
 export default {
     data() {
         return {
-            color: []
+            aVar: a,
+            inputText: 'saldknlakhsd',
+            checkboxes: [
+                {
+                    label: 'Белый',
+                    id: 1,
+                    checked: false,
+                },
+                {
+                    label: 'Черный',
+                    id: 2,
+                    checked: true,
+                },
+                {
+                    label: 'Синий',
+                    id: 3,
+                    checked: false,
+                },
+                {
+                    label: 'Красный',
+                    id: 4,
+                    checked: true,
+                },
+                {
+                    label: 'Розовый',
+                    id: 5,
+                    checked: false,
+                },
+            ],
+            firstName: 'Женя',
+            lastName: 'Фесюк',
+            middleName: 'Владимирович',
         }
+    },
+    computed: {
+        fio() {
+            return `${this.lastName} ${this.firstName} ${this.middleName}`
+            // return this.lastName + ' ' + this.firstName + ' ' + this.middleName
+        }
+    },
+    methods: {
+        updateInput(e) {
+            this.inputText = e.target.value
+        }
+    },
+    components: {
+        VCheckbox,
     }
 }
 </script>
 
 <style scoped lang="scss">
-.real-checkbox {
-    width: 0;
-    height: 0;
-    opacity: 0;
-    position: absolute;
-    z-index: -1;
-}
-
-.real-checkbox:focus + .custom-checkbox {
-    box-shadow: 0px 0px 0px 1px #000000, 0px 0px 10px #000000;
-}
-
-.custom-checkbox {
-    position: relative;
-    display: inline-block;
-    width: 20px;
-    height: 20px;
-    background: #ffffff;
-    border: 2px solid #302d2d;
-    border-radius: 4px;
-    vertical-align: sub;
-    margin-right: 5px;
-}
-
-.custom-checkbox::before {
-    content: '';
-
-    display: inline-block;
-    width: 10px;
-    height: 10px;
-
-    background-image: url(@/assets/VectorCheckbox.svg);
-    background-size: contain;
-    background-repeat: no-repeat;
-
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%) scale(0);
-    margin-top: 1px;
-    transition: 0.2s ease-in;
-}
-
-.real-checkbox:checked + .custom-checkbox::before {
-    transform: translate(-50%, -50%) scale(1);
+.checkbox-wrapper{
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    gap: 10px;
+    flex-direction: column;
 }
 </style>
